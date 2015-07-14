efcd6130a38ee41ca87dd43cc590308b25bd500c
paul-kline <pauliankline@gmail.com>
2015-07-13 19:12:25 -0500

    added scripting files for appraisal internal

--- appraisal/sandboxinit.sh
+++ appraisal/sandboxinit.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+
 cabal sandbox delete
 cabal sandbox init
-cabal sandbox add-source ../negotiationtools/
+
+vch=`locate xeVchan/VChanUtil -n1`
+cabal install $vch
+
+tpm=`locate tpmEmulator/tpm-emulator.cabal -n1`
+cabal install $tpm
+
+shar=`locate protocolImplementation/shared/sharedarmor.cabal | pwd`
+cabal install shar 
+
 
 cabal configure
 cabal build
-cabal install
+# cabal install
